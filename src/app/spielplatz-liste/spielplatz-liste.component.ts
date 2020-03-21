76b7a85d71a9d5865c8ab4b3aded5d93f924cef9
KrypTom143 <mail@wehlan.biz>
2020-03-21 22:45:57 +0100

    Project css -> scss

--- src/app/spielplatz-liste/spielplatz-liste.component.ts
+++ src/app/spielplatz-liste/spielplatz-liste.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-spielplatz-liste',
   templateUrl: './spielplatz-liste.component.html',
-  styleUrls: ['./spielplatz-liste.component.css']
+  styleUrls: ['./spielplatz-liste.component.scss']
 })
 export class SpielplatzListeComponent implements OnInit {
 
