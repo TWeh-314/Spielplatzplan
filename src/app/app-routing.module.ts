d42a0b1abd9dc8b0e371f702352a488fa307f385
KrypTom143 <mail@wehlan.biz>
2020-03-22 00:39:39 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SpielplatzListeComponent } from './spielplatz-liste/spielplatz-liste.component';
+
+const routes: Routes = [
+  { path: 'Liste',  component: SpielplatzListeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
