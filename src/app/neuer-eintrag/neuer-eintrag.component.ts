e304ce3b2dcf9f8d38d06f933ff2080a19db44df
KrypTom143 <mail@wehlan.biz>
2020-03-22 01:52:47 +0100

    ReactiveForm for new entries

--- src/app/neuer-eintrag/neuer-eintrag.component.ts
+++ src/app/neuer-eintrag/neuer-eintrag.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Spielplatz } from './../shared/spielplatz';
 
 @Component({
   selector: 'app-neuer-eintrag',
   templateUrl: './neuer-eintrag.component.html',
   styleUrls: ['./neuer-eintrag.component.scss']
 })
-export class NeuerEintragComponent implements OnInit {
+export class NeuerEintragComponent {
 
-  constructor() { }
+  @Output() created = new EventEmitter<Spielplatz>();
 
-  ngOnInit() {
+  eintragForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    beschreibung: new FormControl('', Validators.required),
+    link: new FormControl('')
+  });
+
+  isInvalid(name: string) {
+    const control = this.eintragForm.get(name);
+    return control.invalid && control.dirty;
   }
 
+  submitForm() {
+    const neuerEintrag = {
+      ...this.eintragForm.value,
+      rating: 1
+    };
+
+    this.created.emit(neuerEintrag);
+    this.eintragForm.reset();
+  }
 }
